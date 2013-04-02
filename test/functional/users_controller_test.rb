fb8e667c52df6f50443e80bce8c73185da0b68a4
Oleg Chebulaev <chebulaev.oleg@gmail.com>
2013-04-02 14:47:28 +0400

    add functional tests for users controller

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create user" do
+    assert_difference('User.count') do
+      post :create, user: {email: @user.email, password: @user.password_digest, password_confirmation: @user.password_digest}
+    end
+
+    assert_equal 'Signed up!', flash[:notice]
+    assert_redirected_to root_url
+  end
 end
